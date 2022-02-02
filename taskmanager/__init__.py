ab7e97e11f5e0e3e320d828e05328e8b48a62209
davewatters <89341297+davewatters@users.noreply.github.com>
2022-02-02 11:12:58 +0000

    add config to disable track mod deprecation warning

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -8,7 +8,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
 
 db = SQLAlchemy(app)
 
