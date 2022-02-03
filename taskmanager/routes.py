b5f797ff8292b70283625417bc2848202eb2613e
davewatters <89341297+davewatters@users.noreply.github.com>
2022-02-03 22:45:45 +0000

    add full crud for categorires

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
+
 @app.route("/")
 def home():
     return render_template("tasks.html") 
@@ -9,7 +10,10 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    print(type(categories))
+    print(categories)
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
@@ -20,3 +24,25 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html") 
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    # if request.method == "POST":
+    #     db.session.update(category)
+    #     db.session.commit()
+    #     return redirect(url_for("categories"))
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category) 
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
