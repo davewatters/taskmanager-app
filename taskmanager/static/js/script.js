3d1f0496ff05934df0351d1057efa89c40dffed0
davewatters <89341297+davewatters@users.noreply.github.com>
2022-02-08 21:58:19 +0000

    feat: add add_task function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,7 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
   M.Sidenav.init(sidenav);
 });
 
+// Modal to confirm delete category
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
   var instances = M.Modal.init(elems);
+
+// datepicker initialization
+let datepicker = document.querySelectorAll(".datepicker");
+M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+});
+
+// select initialization
+let selects = document.querySelectorAll("select");
+M.FormSelect.init(selects); 
 });
