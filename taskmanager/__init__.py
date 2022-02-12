ef55db153210b927fdbe5ad8fd44a68224aa88a4
davewatters <89341297+davewatters@users.noreply.github.com>
2022-02-12 15:56:47 +0000

    update __init__.py to check for dev environment

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,8 +6,12 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
 
 db = SQLAlchemy(app)
