d9f770308ce5fccd0efdc36bdf4f71619c69592b
davewatters <89341297+davewatters@users.noreply.github.com>
2022-02-12 16:17:52 +0000

    fix __init__.py: database uri not assigned

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -15,7 +15,8 @@ else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
