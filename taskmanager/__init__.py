1edb98501e3295269e68050eba207f795371650e
davewatters <89341297+davewatters@users.noreply.github.com>
2022-02-12 16:06:48 +0000

    update __init_py to fix heroku psql uri

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
@@ -7,8 +8,13 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 if os.environ.get("DEVELOPMENT") == "True":
+    # Lcoal
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
+    # Heroku
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
